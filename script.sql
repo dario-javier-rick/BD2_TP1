5fc9142648e5466089f886b7f9330e1608502ff2
Dario Javier Rick <dario_rick@outlook.com>
2018-09-25 07:12:43 -0300

    Inicio funcion get_disk_page

--- script.sql
+++ script.sql
@@ -24,3 +24,39 @@ VALUES	(1,FALSE,FALSE,1,NULL),
         (2,FALSE,FALSE,2,NULL),
         (2,FALSE,FALSE,3,NULL),
         (2,FALSE,FALSE,4,NULL);
+
+
+----------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION get_disk_page (nro_page INTEGER)
+  RETURNS INTEGER AS
+$BODY$
+DECLARE
+  contador INTEGER := 0 ;
+  resultado REAL := _numero;
+
+BEGIN
+
+	RAISE NOTICE 'Se invoca get_disk_page(%)', _nro_page;
+
+  LOOP
+  EXIT WHEN contador = _cantidad ;
+    contador := contador + 1 ;
+    resultado := resultado + (resultado * _porcentaje / 100);
+  END LOOP ;
+
+	RETURN resultado;
+
+	EXCEPTION
+	WHEN OTHERS THEN
+		RAISE NOTICE 'Ocurrió un error general en get_disk_page() ';
+		RAISE NOTICE '% %', SQLERRM, SQLSTATE;
+	RETURN NULL;
+
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+-- select get_disk_page(1);
+
+----------------------------------------------------------------------------------------
